7f1d5495991d62104d46f172e87d6a93697bf251
Michael Colburn <mac002@theColburns.us>
2017-12-18 11:32:01 -0500

    a

---
+++ resetLash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git rm actors_en_UK_lash.ares
+git add *
+git commit -m "a"
+git push origin
+git cp actors_en_US_dedes.ares actors_en_UK_lash.ares
+git add *
+git commit -m "a"
+git push origin
+
